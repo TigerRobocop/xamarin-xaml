b53c029bdff0865157a26a93a381a8ea37e92649
Aluno <liviarsouza@gmail.com>
2018-09-29 15:58:21 -0300

    timer app

--- HelloXaml/MainPage.xaml.cs
+++ HelloXaml/MainPage.xaml.cs
@@ -4,14 +4,36 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using Xamarin.Forms;
+using System.Timers;
+//using System.Diagnostics;
 
 namespace HelloXaml
 {
     public partial class MainPage : ContentPage
     {
+            bool isRunning = false;
+
         public MainPage()
         {
             InitializeComponent();
+
+            lblTimer.Text = DateTime.Now.ToString("T");
+            Device.StartTimer(TimeSpan.FromSeconds(1), HandleFunc);
         }
+
+        void Handle_Clicked(object sender, System.EventArgs e)
+        {
+            isRunning = !isRunning;
+            btnTimer.Text = isRunning ? "Stop" : "Start";
+        }
+
+        bool HandleFunc()
+        {
+            if(isRunning) {
+                lblTimer.Text = DateTime.Now.ToString("T");
+            }
+            return true;
+        }
+
     }
 }
